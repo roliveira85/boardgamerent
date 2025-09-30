bf74a16bd7ab41d3536cdf13f4e3c6182e683eb7
Rafael Ribeiro Oliveira <rafa.dmaster@gmail.com>
2025-09-29 22:57:36 -0300

    Ajuste de configurações no settings

--- djangoapp/projeto/settings.py
+++ djangoapp/projeto/settings.py
@@ -9,23 +9,28 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+#arquivos estáticos /data/web
+#arquivos de media  /data/media
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
+MEDIA_DIR = BASE_DIR.parent / 'data' / 'media'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^!%q2%kxh73ex_a!i0*!w)-_@$&v20y7fl%2ql*3#y34+nqsjt'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+    if h.strip()
+]
 
 
 # Application definition
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : os.getenv('DB_ENGINE','change-me'),
+        'NAME' : os.getenv('POSTGRES_DB','change-me'),
+        'USER' : os.getenv('POSTGRES_USER','change-me'),
+        'PASSWORD' : os.getenv('POSTGRES_PASSWORD','change-me'),
+        'HOST' : os.getenv('POSTGRES_HOST','change-me'),
+        'PORT' : os.getenv('POSTGRES_PORT','change-me'),
     }
 }
 
@@ -102,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -114,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = DATA_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = DATA_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
